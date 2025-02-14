a6574e0a2d05e7e296236bf17ebd08dd5561de02
onixmannager <56134178+onixmannager@users.noreply.github.com>
2025-02-14 23:08:12 +0100

    Update dashboard.js

--- dashboard.js
+++ dashboard.js
@@ -49,7 +49,7 @@ onAuthStateChanged(auth, async (user) => {
         const referidosContados = data.referidosContados || [];
 
         if (data.codigoAfiliado) {
-          const linkAfiliado = `https://cinonix.vercel.app/?afiliado=${data.codigoAfiliado}`;
+          const linkAfiliado = `https://cinonix.vercel.app/005confirmacion.html/?afiliado=${data.codigoAfiliado}`;
           document.getElementById("linkAfiliado").textContent = linkAfiliado;
           document.getElementById("linkAfiliado").href = linkAfiliado;
         } else {
