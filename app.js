fb4fabe5eac54566f7b9b9e19c36cc149ae8ce96
onixmannager <56134178+onixmannager@users.noreply.github.com>
2026-04-09 01:37:24 +0200

    Update app.js

--- app.js
+++ app.js
@@ -9,7 +9,8 @@ import {
   signInWithEmailAndPassword, 
   sendPasswordResetEmail, 
   onAuthStateChanged, 
-  signOut 
+  signOut,
+  sendEmailVerification
 } from "https://www.gstatic.com/firebasejs/11.3.0/firebase-auth.js";
 import { 
   getFirestore, 
@@ -57,6 +58,7 @@ window.registrarUsuario = async function(email, password, refParam) {
   try {
     const userCredential = await createUserWithEmailAndPassword(auth, email, password);
     const user = userCredential.user;
+    await sendEmailVerification(user);
 
     // Prioridad: 1) param del formulario, 2) URL, 3) localStorage
     const codigoReferido = refParam || obtenerCodigoReferido() || null;
